e35cbb7652e9fa580570b4d25f2ff7b437c60f56
qinyanjuidavid <davidkinyanjui052@gmail.com>
2020-04-16 08:29:49 -0700

    On branch master

--- Estimator/myapp/views.py
+++ Estimator/myapp/views.py
@@ -15,10 +15,28 @@ def Home(request):
             timeToElapse=form.cleaned_data['timeToElapse']
             population=form.cleaned_data['population']
             totalHospitalBeds=form.cleaned_data['totalHospitalBeds']
+            #challenge 1
+            currentlyInfected=reportedCases*10
+            currentlyInfectedsevere=reportedCases*50
+            time=period/3
+            actualtime=2*round(time,0)
+            InfectionByRequestedTime=currentlyInfected*actualtime
+            InfectionByRequestedTimeSevere=currentlyInfectedsevere*actualtime
+            #challenge 2
+            severeCasesByRequestedTime_impact=InfectionByRequestedTime*0.15
+            severeCasesByRequestedTime_severe=InfectionByRequestedTimeSevere*0.15
+
+
     else:
         form=Covid19Form()
 
     context={
-    'form':form
+    'form':form,
+    'currentlyInfected':currentlyInfected,
+    'currentlyInfectedsevere':currentlyInfectedsevere,
+    'InfectionByRequestedTime':InfectionByRequestedTime,
+    'InfectionByRequestedTimeSevere':InfectionByRequestedTimeSevere,
+    'severeCasesByRequestedTime_impact':severeCasesByRequestedTime_impact,
+    'severeCasesByRequestedTime_severe':severeCasesByRequestedTime_severe,
     }
     return render(request,'myapp/home.html',context)
